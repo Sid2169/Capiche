7a6f9feb33ba50d182f9c1ff74093f235413e0ce
Sid2169 <siddharthasingh21109572@gmail.com>
2025-11-21 16:52:19 +0530

    Add initial files and dev configuration

--- webpack.common.js
+++ webpack.common.js
@@ -1,37 +1,42 @@
-// webpack.prod.js
 const path = require("path");
+
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
+  entry: "./src/app.js",
+
   output: {
-    filename: "main.js",
+    filename: "app.js",
+
     path: path.resolve(__dirname, "dist"),
+
     clean: true,
   },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
+
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html",
+      filename: "index.html",
+
+      template: "./src/index.html",
     }),
   ],
+
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
         test: /\.html$/i,
+
         loader: "html-loader",
       },
+
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
         type: "asset/resource",
+
+        generator: {
+          filename: "images/img_[hash][ext][query]",
+        },
       },
     ],
   },
